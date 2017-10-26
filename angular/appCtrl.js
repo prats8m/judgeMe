1c64df77d5b4180a8d99761f91dde1b97083dea2
prateek singh <prateek.singh@vvdntech.com>
2017-10-26 15:02:32 +0530

    validation

--- angular/appCtrl.js
+++ angular/appCtrl.js
@@ -261,7 +261,7 @@ app.controller('appCtrl', function ($scope, $http, $rootScope, toastr, $location
         $scope.data = {};
         $scope.data.qid = $scope.questionData.id;
         $scope.data.aid = $scope.radvalue.value;
-        if($scope.data.aid){
+        if($scope.radvalue.value){
         commonSetHTTPService2('Post', $scope.data, 'main/save_your_response', function (result) {
             if (count == 10) {
               $rootScope.link2=1
