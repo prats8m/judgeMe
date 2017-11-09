f8c58046d1a35e7e4aa560082d303a61a986c8e6
prateek singh <prateek.singh@vvdntech.com>
2017-11-10 00:25:17 +0530

    bug

--- angular/appCtrl.js
+++ angular/appCtrl.js
@@ -158,6 +158,7 @@ app.controller('appCtrl', function ($scope, $http, $rootScope, toastr, $location
         $('#1'+$scope.answerData.a_id).hide();
         $('#1'+$scope.aid).hide(); 
          $scope.disable = 0;
+         $scope.disable2 = 0;
         $scope.question = {};
         $scope.option = {};
         $('#loader').show();
@@ -247,6 +248,7 @@ app.controller('appCtrl', function ($scope, $http, $rootScope, toastr, $location
         $scope.data.aid = $scope.radvalue.value;
         $scope.data.fid = $rootScope.fid;
         $scope.data.result = $scope.result;
+        $scope.disable2 = 0;
         commonSetHTTPService2('Post', $scope.data, 'main/save_response', function (result) {
             if (count == 10) {
                 $rootScope.done = 1;
